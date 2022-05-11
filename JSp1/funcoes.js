28b497c05ae38b93b44c34a5d552617747e6bd80
Gabriel Carvalho <gabriel.carvalho@aluno.unievangelica.edu.br>
2022-05-11 12:05:20 -0300

    80%

---
+++ JSp1/funcoes.js
@@ -0,0 +1,20 @@
+// ------- DECLARAÇÃO DE FUNÇÃO ---------
+
+// 1) DECLARA A FUNÇAÕ
+                    //STRING
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) EXECUTA A FUNÇÃO (1 OU + VEZES)
+
+imprimeTexto(soma())
+imprimeTexto("outro texto")
+
+// ------- FUNÇÃO SEM PARÂMETRO -------
+
+function soma() {
+    let oi = "oi2"
+    return oi
+}
+
---
+++ JSp1/parametros.js
@@ -0,0 +1,8 @@
+// ------- PARÂMETRO DE FUNÇÃO -------
+
+              //2    //2
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+console.log(soma(2 , 2))
