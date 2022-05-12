2db9d5bc63682e6344d037a950c1067701d35fa8
Gabriel Carvalho <gabriel.carvalho@aluno.unievangelica.edu.br>
2022-05-12 15:11:57 -0300

    Assinatura

---
+++ JS/JSp2/deletar-notas.js
@@ -0,0 +1,22 @@
+const teste = ['sera', 'que', 5]
+const notas = [10, 6, 7.5, 8, 10]
+notas.reverse()
+
+console.log(notas)
+console.log(teste)
+console.log(teste[0])
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+let nome = "Gabriel"
+
+console.log(`
+                █████████               █████████
+                █▄█████▄█               █▄█████▄█
+                █▼▼▼▼▼                     ▼▼▼▼▼█
+                █        AUTOR DO CÓDIGO:       █
+                █  github.com/segabrielcarvalho █
+                █▲▲▲▲▲                     ▲▲▲▲▲█
+                █████████               █████████
+                ██ ██                       ██ ██
+
+ `)
