f479e59fac6c1f0ab652efeb43fb4f8882f1fcaa
Gabriel Carvalho <gabriel.carvalho@aluno.unievangelica.edu.br>
2022-05-13 15:03:15 -0300

    .

---
+++ JS/JSp2/atividade.js
@@ -0,0 +1,5 @@
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"]
+const arrayConcat = arrayOriginal.concat("André", "Fernanda")
+
+console.log(arrayOriginal)
+console.log(arrayConcat)
