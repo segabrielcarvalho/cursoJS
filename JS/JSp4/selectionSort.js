cab949b80cd74094643dacb2005fbcff608ccb66
Gabriel Carvalho <gabriel.carvalho@aluno.unievangelica.edu.br>
2022-05-25 11:59:44 -0300

    Algoritmos

---
+++ JS/JSp4/selectionSort.js
@@ -0,0 +1,13 @@
+const menorValor = require('./menorValor')
+const livros = require('./listalivros')
+
+for (let atual = 0; atual < livros.length; atual++) {
+    let menor = menorValor(livros, atual)
+    let livroAtual = livros[atual]
+    let livroMenorPreco = livros[menor]
+
+    livros[atual] = livroMenorPreco
+    livros[menor] = livroAtual
+}
+
+console.log(livros)
